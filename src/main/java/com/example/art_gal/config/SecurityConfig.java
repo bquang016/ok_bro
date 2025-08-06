b6cfb1ff728c9b7d90c4751a22f3a4e4e75d3d95
bquang016 <buidangquangmtp@gmail.com>
2025-08-06 15:47:35 +0700

    add

--- src/main/java/com/example/art_gal/config/SecurityConfig.java
+++ src/main/java/com/example/art_gal/config/SecurityConfig.java
@@ -39,9 +39,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        // *** DÒNG QUAN TRỌNG ĐƯỢC THÊM VÀO ĐÂY ***
                         .requestMatchers("/actuator/**").permitAll()
-                        // ======================================
                         .requestMatchers("/api/files/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(
